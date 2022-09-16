2250a5f5cb6a4d707075ac34ab125fa995089fa1
pmunuera <pmunueragarcia.cf@iesesteveterradas.cat>
2022-09-16 18:11:06 +0200

    Commit creación

---
+++ app/src/main/java/com/example/app1/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.app1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Context context = getApplicationContext();
+        CharSequence text = "Hello toast!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+        setContentView(R.layout.content_layout_id);
+
+        /*final Button button = findViewById(R.id.button_id);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+            }
+        });*/
+
+    }
+}
